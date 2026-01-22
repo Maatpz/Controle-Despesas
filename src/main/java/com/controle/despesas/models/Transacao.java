492cf183bae91f1ac795d71dc720f3bd595ae2c1
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-01-22 00:26:40 -0300

    feat: update transacao

--- src/main/java/com/controle/despesas/models/Transacao.java
+++ src/main/java/com/controle/despesas/models/Transacao.java
@@ -1,14 +1,16 @@
 package com.controle.despesas.models;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.controle.despesas.models.enums.Tipo;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,6 +19,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,22 +40,26 @@ public class Transacao {
     @Positive(message = "Valor deve ser positivo")
     @Column(nullable = false)
     private Double valor;
-
+    
+    @Size(max = 255, message = "Descricao deve ter no maximo 255 caracteres")
     private String descricao;
 
     
     @NotNull(message = "Tipo e obrigatorio")
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private String tipo;
+    private Tipo tipo;
 
     
-    @Column(nullable = false, name = "data_transaçao")
-    @CreationTimestamp
+    @NotNull(message = "Data e obrigatoria")
+    @Column(nullable = false, name = "data_transacao")
     private LocalDateTime data;
 
     @ManyToOne
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
+
+    
 
     @Column(name = "data_criacao")
     @CreationTimestamp
@@ -61,7 +68,7 @@ public class Transacao {
     
     @Column(name = "data_atualizacao")
     @UpdateTimestamp
-    private LocalDateTime data_atualizacao;
+    private LocalDateTime dataAtualizacao;
 
 
 }
