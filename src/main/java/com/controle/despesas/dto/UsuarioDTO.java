b37a2fa04939d0d521b2387c012970063849ab69
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-01-21 22:42:48 -0300

    feat: criar usuariodto

---
+++ src/main/java/com/controle/despesas/dto/UsuarioDTO.java
@@ -0,0 +1,35 @@
+package com.controle.despesas.dto;
+
+import java.util.UUID;
+
+import com.controle.despesas.models.enums.Role;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+
+    private UUID id;
+
+    @NotBlank(message = "Nome e obrigatorio")
+    private String nome;
+
+    @NotBlank(message = "Email e obrigatorio")
+    @Email(message = "Email invalido")
+    private String email;
+
+    @NotBlank(message = "Senha e obrigatoria")
+    private String senha;
+
+    private String role;
+
+    private Double saldoEconomia;
+
+    
+}
