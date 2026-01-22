a94fcd07300833bc52eebb0b938b1db5790d464f
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-01-22 09:39:41 -0300

    feat: criar usuariorequest

---
+++ src/main/java/com/controle/despesas/dto/request/UsuarioRequest.java
@@ -0,0 +1,26 @@
+package com.controle.despesas.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioRequest {
+
+    @NotBlank(message = "Nome e obrigatorio")
+    @Size(max = 50, message = "Nome deve ter no maximo 50 caracteres")
+    private String nome;
+
+    @NotBlank(message = "Email e obrigatorio")
+    @Email(message = "Email invalido")
+    private String email;
+
+    @NotBlank(message = "Senha e obrigatoria")
+    @Size(min = 6, message = "Senha deve ter no minimo 6 caracteres")
+    private String senha;
+}
